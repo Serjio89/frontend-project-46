03919dd5485f72a05baf09e0a0156eeac4aa331a
Rubtsov Sergey <rubtsov.sergey@hotmail.com>
2023-03-02 20:33:05 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,13 +2,15 @@
 import { Command } from 'commander';
 
 const program = new Command();
+
 program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
-  .option('-f, --format <type>', 'output format')
+  .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'output usage information')
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
   });
-program.parse();
+program.parse(process.argv);
+
