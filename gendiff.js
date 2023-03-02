a52b4bb364ba745c73ac5a1a0cf79e737103ea26
Rubtsov Sergey <rubtsov.sergey@hotmail.com>
2023-03-02 18:47:45 +0300

    step1 modify

--- gendiff.js
+++ gendiff.js
@@ -5,5 +5,7 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .name('gendiff')
-  .version('0.4.0')
+  .version('10.0.0')
+  .option('-f, --format <type>',  'output format')
+  .helpOption('-h, --help', 'output usage information')
   .parse(process.argv);
