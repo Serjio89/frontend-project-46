c03c48d076a2c9e1c7c02e1b34c90ede51f166c2
Rubtsov Sergey <rubtsov.sergey@hotmail.com>
2023-03-09 11:44:18 +0300

    gendiff text correction

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,12 +6,13 @@ const program = new Command();
 
 program
   .version('1.0.0')
+  .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
     const result = genDiff(filepath1, filepath2, program.opts().format);
     console.log(result);
   });
+
 program.parse();
