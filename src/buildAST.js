d41e3df524115ebe354a6d386c672142462adf1e
Rubtsov Sergey <rubtsov.sergey@hotmail.com>
2023-03-23 16:13:34 +0300

    some refactoring for codeclimate

--- src/buildAST.js
+++ src/buildAST.js
@@ -6,30 +6,27 @@ const getDifference = (data1, data2) => {
   const unionKeys = _.union(keys1, keys2);
   const sortedKeys = _.sortBy(unionKeys);
 
-  return sortedKeys.map((key) => {
+  const result = sortedKeys.map((key) => {
+    let difference;
+
     if (!_.has(data1, key)) {
-      return { key, type: 'added', value: data2[key] };
+      difference = { key, type: 'added', value: data2[key] };
+    } else if (!_.has(data2, key)) {
+      difference = { key, type: 'deleted', value: data1[key] };
+    } else if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      difference = { key, type: 'nested', children: getDifference(data1[key], data2[key]) };
+    } else if (_.isEqual(data1[key], data2[key])) {
+      difference = { key, type: 'unchanged', value: data1[key] };
+    } else {
+      difference = {
+        key, type: 'changed', value1: data1[key], value2: data2[key],
+      };
     }
 
-    if (!_.has(data2, key)) {
-      return { key, type: 'deleted', value: data1[key] };
-    }
-
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return { key, type: 'nested', children: getDifference(data1[key], data2[key]) };
-    }
-
-    if (_.isEqual(data1[key], data2[key])) {
-      return { key, type: 'unchanged', value: data1[key] };
-    }
-
-    return {
-      key,
-      type: 'changed',
-      value1: data1[key],
-      value2: data2[key],
-    };
+    return difference;
   });
+
+  return result;
 };
 
 export default getDifference;
