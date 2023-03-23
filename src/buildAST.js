41877c35784c432d6baed1b9db0648d045707ca9
Rubtsov Sergey <rubtsov.sergey@hotmail.com>
2023-03-23 16:34:30 +0300

    some refactoring for codeclimate

--- src/buildAST.js
+++ src/buildAST.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 
+const getValueForKey = (key, data) => (_.has(data, key) ? data[key] : undefined);
+
 const getDifference = (data1, data2) => {
   const keys1 = _.keys(data1);
   const keys2 = _.keys(data2);
@@ -7,23 +9,24 @@ const getDifference = (data1, data2) => {
   const sortedKeys = _.sortBy(unionKeys);
 
   const result = sortedKeys.map((key) => {
-    let difference;
+    const value1 = getValueForKey(key, data1);
+    const value2 = getValueForKey(key, data2);
 
-    if (!_.has(data1, key)) {
-      difference = { key, type: 'added', value: data2[key] };
-    } else if (!_.has(data2, key)) {
-      difference = { key, type: 'deleted', value: data1[key] };
-    } else if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      difference = { key, type: 'nested', children: getDifference(data1[key], data2[key]) };
-    } else if (_.isEqual(data1[key], data2[key])) {
-      difference = { key, type: 'unchanged', value: data1[key] };
-    } else {
-      difference = {
-        key, type: 'changed', value1: data1[key], value2: data2[key],
+    if (value1 === undefined) {
+      return { key, type: 'added', value: value2 };
+    }
+    if (value2 === undefined) {
+      return { key, type: 'deleted', value: value1 };
+    }
+    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+      return { key, type: 'nested', children: getDifference(value1, value2) };
+    }
+    if (!_.isEqual(value1, value2)) {
+      return {
+        key, type: 'changed', value1, value2,
       };
     }
-
-    return difference;
+    return { key, type: 'unchanged', value: value2 };
   });
 
   return result;
