1551b5584fadebd9281dda8cc474d1f8ff407a75
Rubtsov Sergey <rubtsov.sergey@hotmail.com>
2023-03-25 18:02:32 +0300

    step 8 tests refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,19 +7,24 @@ const stylish = readFileSync('__fixtures__/expected_file_stylish.txt', 'utf-8');
 const plain = readFileSync('__fixtures__/expected_file_plain.txt', 'utf-8');
 const json = readFileSync('__fixtures__/expected_file_json.txt', 'utf-8');
 
-const formats = ['json', 'yaml', 'yml'];
-
-test.each(formats)('stylish test - %s format', (format) => {
-  expect(genDiff(`file1.${format}`, `file2.${format}`)).toBe(stylish);
+test('stylish test', () => {
+  expect(genDiff('file1.json', 'file2.json')).toBe(stylish);
+  expect(genDiff('file1.yaml', 'file2.yaml')).toBe(stylish);
+  expect(genDiff('file1.yml', 'file2.yml')).toBe(stylish);
 });
 
-test.each(formats)('plain test - %s format', (format) => {
-  expect(genDiff(`file1.${format}`, `file2.${format}`, 'plain')).toBe(plain);
+test('plain test', () => {
+  expect(genDiff('file1.json', 'file2.json', 'plain')).toBe(plain);
+  expect(genDiff('file1.yaml', 'file2.yaml', 'plain')).toBe(plain);
+  expect(genDiff('file1.yml', 'file2.yml', 'plain')).toBe(plain);
 });
 
-test.each(formats)('json test - %s format', (format) => {
-  expect(genDiff(`file1.${format}`, `file2.${format}`, 'json')).toBe(json);
+test('json test', () => {
+  expect(genDiff('file1.json', 'file2.json', 'json')).toBe(json);
+  expect(genDiff('file1.yaml', 'file2.yaml', 'json')).toBe(json);
+  expect(genDiff('file1.yml', 'file2.yml', 'json')).toBe(json);
 });
+
 test('errors test', () => {
   expect(() => (parser('data', 'error'))).toThrow('not supported');
   expect(() => (formatter('data', 'error'))).toThrow('not supported');
